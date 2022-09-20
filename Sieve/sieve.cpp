45a50f9dcc9c593022f5897155a793c00b055d6b
RKPurple <r.kallur.21@gmail.com>
2022-09-19 22:56:31 -0400

    init

--- Sieve/sieve.cpp
+++ Sieve/sieve.cpp
@@ -6,22 +6,25 @@
  * Pledge      : I pledge my honor that I have abided by the Stevens Honor System
  ******************************************************************************/
 #include <cmath>
-#include <iomanip>  
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <cstring>
 
 using namespace std;
 
-class PrimesSieve {
+class PrimesSieve
+{
 public:
     PrimesSieve(int limit);
 
-    ~PrimesSieve() {
-        delete [] is_prime_;
+    ~PrimesSieve()
+    {
+        delete[] is_prime_;
     }
 
-    int num_primes() const {
+    int num_primes() const
+    {
         return num_primes_;
     }
 
@@ -29,7 +32,7 @@ public:
 
 private:
     // Instance variables
-    bool * const is_prime_;
+    bool *const is_prime_;
     const int limit_;
     int num_primes_, max_prime_, max_prime_width_, primes_per_row;
 
@@ -39,13 +42,14 @@ private:
     static int num_digits(int num);
 };
 
-PrimesSieve::PrimesSieve(int limit) :
-    is_prime_{new bool[limit + 1]}, limit_{limit} {
-    memset(is_prime_, 1, limit + 1); //Initialize the elements of is_prime_ to True except for the final value
+PrimesSieve::PrimesSieve(int limit) : is_prime_{new bool[limit + 1]}, limit_{limit}
+{
+    memset(is_prime_, 1, limit + 1); // Initialize the elements of is_prime_ to True except for the final value
     sieve();
     num_primes_ = count_num_primes();
     int i = limit_;
-    while (!is_prime_[i]) { // Iterate through is_prime_ backward to find largest prime
+    while (!is_prime_[i])
+    { // Iterate through is_prime_ backward to find largest prime
         i--;
     }
     max_prime_ = i;
@@ -53,72 +57,93 @@ PrimesSieve::PrimesSieve(int limit) :
     primes_per_row = 80 / (max_prime_width_ + 1);
 }
 
-void PrimesSieve::display_primes() const {
+void PrimesSieve::display_primes() const
+{
     // TODO: write code to display the primes in the format specified in the
     // requirements document.
     int count = 0;
     bool first = 1;
-    if (primes_per_row > num_primes_) {
-        for (int i = 2; i <= limit_; i++) {
-        if (is_prime_[i]) {
-            if (first) {
-                cout << i;
-                first = 0;
+    if (primes_per_row > num_primes_)
+    {
+        for (int i = 2; i <= limit_; i++)
+        {
+            if (is_prime_[i])
+            {
+                if (first)
+                {
+                    cout << i;
+                    first = 0;
+                }
+                else
+                {
+                    cout << setw(num_digits(i) + 1) << i;
+                }
             }
-            else {
-                cout << setw(num_digits(i) + 1) << i;
-            }
-        }
         }
     }
-    else {
-        for (int i = 2; i <= limit_; i++) {
-            if (is_prime_[i]) {
-                if (first) {
+    else
+    {
+        for (int i = 2; i <= limit_; i++)
+        {
+            if (is_prime_[i])
+            {
+                if (first)
+                {
                     cout << setw(max_prime_width_) << i;
                     first = 0;
                     count++;
                 }
-                else {
+                else
+                {
                     cout << setw(max_prime_width_ + 1) << i;
                     count++;
-                        if (count == primes_per_row) {
-                            cout << endl;
-                            count = 0;
-                            first = 1;
-                        }
+                    if (count == primes_per_row)
+                    {
+                        cout << endl;
+                        count = 0;
+                        first = 1;
+                    }
                 }
             }
         }
     }
 }
 
-int PrimesSieve::count_num_primes() const {
+int PrimesSieve::count_num_primes() const
+{
     // TODO: write code to count the number of primes found
     int num_primes = 0;
-    for (int i = 2; i <= limit_; i++) {
-        if (is_prime_[i]) {
+    for (int i = 2; i <= limit_; i++)
+    {
+        if (is_prime_[i])
+        {
             num_primes++;
         }
     }
     return num_primes;
 }
 
-void PrimesSieve::sieve() {
-    for (int i = 2; i <= sqrt(limit_); i++) {
-        if (is_prime_[i]) {
-            for (int j = pow(i, 2); j <= limit_; j+= i) {
+void PrimesSieve::sieve()
+{
+    for (int i = 2; i <= sqrt(limit_); i++)
+    {
+        if (is_prime_[i])
+        {
+            for (int j = pow(i, 2); j <= limit_; j += i)
+            {
                 is_prime_[j] = 0;
             }
         }
     }
 }
 
-int PrimesSieve::num_digits(int num) {
+int PrimesSieve::num_digits(int num)
+{
     // TODO: write code to determine how many digits are in an integer
     // Hint: No strings are needed. Keep dividing by 10.
     int digits = 0;
-    while (num > 9) {
+    while (num > 9)
+    {
         num /= 10;
         digits++;
     }
@@ -126,9 +151,11 @@ int PrimesSieve::num_digits(int num) {
     return digits;
 }
 
-int main() {
-    cout << "**************************** " <<  "Sieve of Eratosthenes" <<
-            " ****************************" << endl;
+int main()
+{
+    cout << "**************************** "
+         << "Sieve of Eratosthenes"
+         << " ****************************" << endl;
     cout << "Search for primes up to: ";
     string limit_str;
     cin >> limit_str;
@@ -138,17 +165,20 @@ int main() {
     istringstream iss(limit_str);
 
     // Check for error.
-    if ( !(iss >> limit) ) {
+    if (!(iss >> limit))
+    {
         cerr << "Error: Input is not an integer." << endl;
         return 1;
     }
-    if (limit < 2) {
+    if (limit < 2)
+    {
         cerr << "Error: Input must be an integer >= 2." << endl;
         return 1;
     }
 
     PrimesSieve x(limit);
-    cout << endl << "Number of primes found: " << x.num_primes() << endl;
+    cout << endl
+         << "Number of primes found: " << x.num_primes() << endl;
     cout << "Primes up to " << limit << ":" << endl;
     x.display_primes();
     // TODO: write code that uses your class to produce the desired output.

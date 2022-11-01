74228f272ebe31f210b9284083fbea7672681163
RKPurple <r.kallur.21@gmail.com>
2022-11-01 17:53:44 -0400

    inversion counter working with python test case creator

--- InversionCounter/inversioncounter.cpp
+++ InversionCounter/inversioncounter.cpp
@@ -1,9 +1,10 @@
 /*******************************************************************************
- * Name          : quickselect.cpp
- * Author        : Rohan Kallur
- * Pledge        : I pledge my honor that I have abided by the Stevens Honor System.
- * Date          : 10/28/22
- * Description   : Implements the quickselect algorithm.
+ * Name        : inversioncounter.cpp
+ * Author      : Rohan Kallur
+ * Version     : 1.0
+ * Date        : 10/31/22
+ * Description : Counts the number of inversions in an array.
+ * Pledge      : I pledge my honor that I have abided by the Stevens Honor System.
  ******************************************************************************/
 #include <iostream>
 #include <algorithm>
@@ -12,12 +13,113 @@
 #include <cstdio>
 #include <cctype>
 #include <cstring>
+
 using namespace std;
+
+// Function prototype.
+static long mergesort(int array[], int scratch[], int low, int high);
+
+/**
+ * Counts the number of inversions in an array in Theta(n^2) time.
+ */
+long count_inversions_slow(int array[], int length)
+{
+    // TODO
+    int num_inversions = 0;
+    // loop over all the elements except for the last
+    for (int i = 0; i < length - 1; ++i)
+    {
+        // check the current element against all after it
+        for (int j = i + 1; j < length; ++j)
+        {
+            // check if the current element is greater than elements after it and add to inversion counter
+            if (array[i] > array[j])
+                ++num_inversions;
+        }
+    }
+    return num_inversions;
+}
+
+/**
+ * Counts the number of inversions in an array in Theta(n lg n) time.
+ */
+long count_inversions_fast(int array[], int length)
+{
+    // TODO
+    // Hint: Use mergesort!
+    int *scratch = new int[length];
+    long num_inversions = mergesort(array, scratch, 0, length - 1);
+    delete[] scratch;
+    return num_inversions;
+}
+
+long merge(int array[], int scratch[], int low, int mid, int high)
+{
+    int i1 = low;
+    int i2 = mid + 1;
+    int i = low;
+    long num_inversions = 0;
+    while (i1 <= mid && i2 <= high)
+    {
+        if (array[i1] <= array[i2])
+        {
+            scratch[i++] = array[i1++];
+        }
+        else
+        {
+            scratch[i++] = array[i2++];
+            num_inversions += (mid - i1 + 1);
+        }
+    }
+    while (i1 <= mid)
+    {
+        scratch[i++] = array[i1++];
+    }
+    while (i2 <= high)
+    {
+        scratch[i++] = array[i2++];
+    }
+    for (i = low; i <= high; i++)
+    {
+        array[i] = scratch[i];
+    }
+    return num_inversions;
+}
+
+static long mergesort(int array[], int scratch[], int low, int high)
+{
+    // TODO
+    long num_inversions = 0;
+    if (low < high)
+    {
+        int mid = low + ((high - low) / 2);
+        num_inversions += mergesort(array, scratch, low, mid);
+        num_inversions += mergesort(array, scratch, mid + 1, high);
+        num_inversions += merge(array, scratch, low, mid, high);
+    }
+    return num_inversions;
+}
 
 int main(int argc, char *argv[])
 {
     // TODO: parse command-line argument
+    if (argc > 2)
+    {
+        cerr << "Usage: " << argv[0] << " [slow]" << endl;
+        return -1;
+    }
+    string s;
+    if (argc == 2)
+    {
+        s = argv[1];
+        if (s != "slow")
+        {
+            cerr << "Error: Unrecognized option '" << argv[1] << "'." << endl;
+            return -1;
+        }
+    }
     cout << "Enter sequence of integers, each followed by a space: " << flush;
+
     istringstream iss;
     int value, index = 0;
     vector<int> values;
@@ -57,6 +159,21 @@ int main(int argc, char *argv[])
             str += c;
         }
     }
+    int num_values = values.size();
+    if (num_values == 0)
+    {
+        cerr << "Error: Sequence of integers not received." << endl;
+        return -1;
+    }
     // TODO: produce output
-    return 0;
+    if (s == "slow")
+    {
+        cout << "Number of inversions: " << count_inversions_slow(&values[0], num_values) << endl;
+        return 1;
+    }
+    else
+    {
+        cout << "Number of inversions: " << count_inversions_fast(&values[0], num_values) << endl;
+        return 1;
+    }
 }
---
+++ InversionCounter/testcasemaker.py
@@ -0,0 +1,7 @@
+import random
+
+def main(n):
+    for i in range(n):
+        print(str(random.randint(-5000,5000)) + " ", end='')
+
+main (100)
